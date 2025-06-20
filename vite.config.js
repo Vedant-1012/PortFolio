1ebe920df2e827dc2610ce7a70b56ed4454dd40a
Vedant Thakkar <vedantthakkar@Vedants-MacBook-Air.local>
2025-06-19 22:45:18 -0500

    Create production build for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Import the 'path' module
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PortFolio/',
-  // Add this 'resolve' object to define the '@' alias
+  // This line is for GitHub Pages deployment
+  base: '/PortFolio/', 
+  // This line is for the build process
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
