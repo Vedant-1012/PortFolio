460bbd5b2e58202d5328a9257f09974ad70c0745
Vedant Thakkar <vedantthakkar@Vedants-MacBook-Air.local>
2025-06-19 23:04:50 -0500

    Create production build for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This line tells your project to work correctly on GitHub Pages
   base: '/PortFolio/',
-  
-  // This line ensures the build process can find your components
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
